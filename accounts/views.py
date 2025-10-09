eb9c90e605e49c280174191b091fd05cbcd96fa7
sofia123776 <sofiahafsa00@gmail.com>
2025-10-09 19:14:44 +0300

    added register and login page

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return redirect('login')
+    return render(request, 'accounts/register.html')
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('home')
